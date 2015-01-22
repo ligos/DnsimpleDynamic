2fa41582f5a573535974359576684ac6fc7cddb7
Murray Grant <mujgrant@ligos.net>
2015-01-22 21:11:12 +1100

    Add Apache license to all files.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DndsUpdate")]
+[assembly: AssemblyTitle("DnsimpleDynamic")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DndsUpdate")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("Murray Grant")]
+[assembly: AssemblyProduct("DnsimpleDynamic")]
+[assembly: AssemblyCopyright("Copyright © Murray Grant 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
